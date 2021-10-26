e60d69aaf56d7d69edc56361a53c6bae99f2d487
Gary Bishop <gary.bishop.unc@gmail.com>
2021-10-26 19:50:06 -0400

    WIP: VSD working.

--- components/vsd.js
+++ components/vsd.js
@@ -2,23 +2,38 @@ import { html, render } from "uhtml";
 import { Base, componentMap } from "./base";
 import { styleString } from "./style";
 
+/** Allow await'ing for a short time
+ * @param {number} ms */
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+/** Wait for a condition to be satisfied
+ * @param {() => boolean} test
+ * @param {number} delay */
+async function waitFor(test, delay = 1) {
+  while (!test()) await sleep(delay);
+}
+
 /**
  * Calculate the actual image size undoing the effects of object-fit
+ * This is async so it can wait for the image to be loaded initially.
  *
  * @param {HTMLImageElement} img
  * */
-function getActualImageSize(img) {
+async function getActualImageSize(img) {
   let left = 0,
     top = 0,
     width = 1,
     height = 1;
   if (img) {
+    // wait for the image to load
+    await waitFor(() => img.complete && img.naturalWidth != 0);
     const cw = img.width,
       ch = img.height,
       iw = img.naturalWidth,
       ih = img.naturalHeight,
       iratio = iw / ih,
       cratio = cw / ch;
+    console.log({ cw, ch, iw, ih, iratio, cratio });
     if (iratio > cratio) {
       width = cw;
       height = cw / iratio;
@@ -30,6 +45,13 @@ function getActualImageSize(img) {
     top = (ch - height) / 2;
   }
   return { left, top, width, height };
+}
+
+function px(v) {
+  return `${v}px`;
+}
+function pct(v) {
+  return `${v}%`;
 }
 
 class VSD extends Base {
@@ -48,7 +70,46 @@ class VSD extends Base {
     const src = items.find((item) => item.details.src).details.src;
     return html`<div class="vsd flex show">
       <img src=${src} />
+      <div
+        class="markers"
+        ref=${(/** @type {HTMLDivElement & { observer: any }} */ node) => {
+          const img = /** @type {HTMLImageElement} */ (
+            node.previousElementSibling
+          );
+          if (!node.observer) {
+            /* get a callback when the image changes size so that we
+             * can resize the div containing the markers to match */
+            node.observer = new ResizeObserver(async () => {
+              const rect = await getActualImageSize(img);
+              node.style.position = "absolute";
+              node.style.left = px(rect.left);
+              node.style.top = px(rect.top);
+              node.style.width = px(rect.width);
+              node.style.height = px(rect.height);
+            });
+            node.observer.observe(img);
+          }
+        }}
+      >
+        ${items
+          .filter((item) => item.label)
+          .map(
+            (item) => html`<button
+              style=${styleString({
+                left: pct(item.details.x),
+                top: pct(item.details.y),
+                width: pct(item.details.w),
+                height: pct(item.details.h),
+                position: "absolute",
+              })}
+              onClick=${rules.handler(this.name, item, "press")}
+            >
+              <span>${item.label}</span>
+            </button>`
+          )}
+      </div>
     </div>`;
   }
 }
+
 componentMap.addMap("vsd", VSD);
