b01e4e7ac0da6b3ccc9fda74506e0355dbceee6d
Graham <graham.trogdon@gmail.com>
2021-12-29 02:30:19 -0500

    issue #4--loading content from google sheet

--- components/content.js
+++ components/content.js
@@ -3,6 +3,7 @@ import { Base } from "./base";
 import db from "../db";
 import { Data } from "../data";
 import XLSX from "xlsx";
+import sheetrock from "sheetrock";
 
 /** @param {File} file */
 async function readLocalSheet(file) {
@@ -44,6 +45,44 @@ async function readLocalSheet(file) {
 export class Content extends Base {
   template() {
     return html` <div class="content">
+      <label for="remoteFileInput">Load a Google Sheets spreadsheet: </label>
+      <input
+        id="remoteFileInput"
+        type="url"
+        required="true"
+        aria-required="true"
+        onkeydown=${async (/** @type {InputEvent} e */ e) => {
+          const target = /** @type {HTMLInputElement} */ (e.target);
+          if (target.checkValidity() && e.key == "Enter") {
+            sheetrock({
+              url: e.target.value,
+              target: document.createElement("div"),
+              callback: (
+                /** @type {object} error */ error,
+                /** @type {object} options */ options,
+                /** @type {object} response */ response
+              ) => {
+                const result = response.rows.reduce((dataArr, rowObj, i) => {
+                  if (i) {
+                    let datum = {};
+                    for (let j = 0; j < rowObj.labels.length; j++) {
+                      datum[rowObj.labels[j].toLowerCase()] =
+                        rowObj.cellsArray[j];
+                      if (!("tags" in datum)) datum["tags"] = [];
+                    }
+                    dataArr.push(datum);
+                  }
+                  return dataArr;
+                }, []);
+
+                this.context.data = new Data(result);
+                this.context.state.update();
+              },
+            });
+          }
+        }}
+      />
+
       <label for="localFileInput">Load a local spreadsheet: </label>
       <input
         id="localFileInput"
@@ -55,6 +94,7 @@ export class Content extends Base {
             "#localLoadStatus"
           ).innerHTML = `Loaded ${result.rows} rows with ${result.fields.length} columns`;
           this.context.data = new Data(result.dataArray);
+          console.log(this.context.data);
           this.context.state.update();
         }}
       />
