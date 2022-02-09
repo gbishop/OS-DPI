276ffc090ce4bdaf01877eca4dce86b3ca125e98
Gary Bishop <gary.bishop.unc@gmail.com>
2022-02-09 11:28:32 -0500

    Read all the sheets in a spreadsheet file.
    Easily allows breaking up content in a logical way.

--- components/content.js
+++ components/content.js
@@ -11,69 +11,70 @@ import pleaseWait from "./wait";
 async function readSheetFromBlob(blob) {
   const data = await blob.arrayBuffer();
   const workbook = XLSX.read(data, { codepage: 65001 });
-  const sheetName = workbook.SheetNames[0];
-  const sheet = workbook.Sheets[sheetName];
-  const range = XLSX.utils.decode_range(sheet["!ref"]);
-  const names = [];
-  const handlers = [];
-  const validColumns = [];
-  // process the header and choose a handler for each column
-  for (let c = range.s.c; c <= range.e.c; c++) {
-    let columnName = sheet[XLSX.utils.encode_cell({ r: 0, c })]?.v;
-    if (typeof columnName !== "string" || !columnName) {
-      continue;
-    }
-    columnName = columnName.toLowerCase();
-    names.push(columnName.trim(" "));
-    validColumns.push(c);
-    switch (columnName) {
-      case "row":
-      case "column":
-      case "page":
-        handlers.push("number");
-        break;
-      default:
-        handlers.push("string");
-        break;
-    }
-  }
-  // Process the rows
   /** @type {Rows} */
   const dataArray = [];
-  for (let r = range.s.r + 1; r <= range.e.r; r++) {
-    /** @type {Row} */
-    const row = {};
-    for (let i = 0; i < validColumns.length; i++) {
-      /** @type {string} */
-      const name = names[i];
-      const c = validColumns[i];
-      let value = sheet[XLSX.utils.encode_cell({ r, c })]?.v;
-      switch (handlers[i]) {
-        case "string":
-          if (typeof value === "undefined") {
-            value = "";
-          }
-          if (typeof value !== "string") {
-            value = value.toString(10);
-          }
-          if (value && typeof value === "string") {
-            row[name] = value;
-          }
+  for (const sheetName of workbook.SheetNames) {
+    const sheet = workbook.Sheets[sheetName];
+    const range = XLSX.utils.decode_range(sheet["!ref"]);
+    const names = [];
+    const handlers = [];
+    const validColumns = [];
+    // process the header and choose a handler for each column
+    for (let c = range.s.c; c <= range.e.c; c++) {
+      let columnName = sheet[XLSX.utils.encode_cell({ r: 0, c })]?.v;
+      if (typeof columnName !== "string" || !columnName) {
+        continue;
+      }
+      columnName = columnName.toLowerCase();
+      names.push(columnName.trim(" "));
+      validColumns.push(c);
+      switch (columnName) {
+        case "row":
+        case "column":
+        case "page":
+          handlers.push("number");
           break;
-        case "number":
-          if (typeof value === "number") {
-            row[name] = Math.floor(value);
-          } else if (value && typeof value === "string") {
-            value = parseInt(value, 10);
-            if (isNaN(value)) {
-              value = 0;
-            }
-            row[name] = value;
-          }
+        default:
+          handlers.push("string");
           break;
       }
     }
-    if (Object.keys(row).length > 0) dataArray.push(row);
+    // Process the rows
+    for (let r = range.s.r + 1; r <= range.e.r; r++) {
+      /** @type {Row} */
+      const row = {};
+      for (let i = 0; i < validColumns.length; i++) {
+        /** @type {string} */
+        const name = names[i];
+        const c = validColumns[i];
+        let value = sheet[XLSX.utils.encode_cell({ r, c })]?.v;
+        switch (handlers[i]) {
+          case "string":
+            if (typeof value === "undefined") {
+              value = "";
+            }
+            if (typeof value !== "string") {
+              value = value.toString(10);
+            }
+            if (value && typeof value === "string") {
+              row[name] = value;
+            }
+            break;
+          case "number":
+            if (typeof value === "number") {
+              row[name] = Math.floor(value);
+            } else if (value && typeof value === "string") {
+              value = parseInt(value, 10);
+              if (isNaN(value)) {
+                value = 0;
+              }
+              row[name] = value;
+            }
+            break;
+        }
+      }
+      if (Object.keys(row).length > 0) dataArray.push(row);
+    }
   }
   return dataArray;
 }
