8e8bc7a921f91043b0d8ff556f361701229bf3b0
Gary Bishop <gary.bishop.unc@gmail.com>
2022-03-30 10:59:00 -0400

    build

--- docs/components/stack.js
+++ docs/components/stack.js
@@ -18,9 +18,21 @@ export class Stack extends Base {
     const style = styleString({
       backgroundColor: this.props.background,
     });
+    /** return the scale of the child making sure it isn't zero or undefined.
+     * @param {Base} child
+     * @returns {number}
+     */
+    function getScale(child) {
+      const SCALE_MIN = 0.1;
+      let scale = +child.props.scale;
+      if (!scale || scale < SCALE_MIN) {
+        scale = SCALE_MIN;
+      }
+      return scale;
+    }
     const empty = this.children.length ? "" : "empty";
     const scaleSum = this.children.reduce(
-      (sum, child) => sum + +child.props.scale,
+      (sum, child) => sum + getScale(child),
       0
     );
     const dimension = this.props.direction == "row" ? "width" : "height";
@@ -33,7 +45,7 @@ export class Stack extends Base {
         (child) =>
           html`<div
             style=${styleString({
-              [dimension]: `${(100 * +child.props.scale) / scaleSum}%`,
+              [dimension]: `${(100 * getScale(child)) / scaleSum}%`,
             })}
           >
             ${child.template()}
