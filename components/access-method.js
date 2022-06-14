a056206380e86ff8b5e2b986d042b15ba313c2ed
Gary Bishop <gary.bishop.unc@gmail.com>
2022-06-14 11:10:38 -0400

    WIP: reorg

--- components/access-method.js
+++ components/access-method.js
@@ -5,44 +5,89 @@ import { Select, Expression, String, Integer } from "./props";
 import Globals from "../globals";
 import db from "../db";
 
-class HandlerCondition extends TreeBase {
-  props = {
-    Condition: new Expression("", { hiddenLabel: true }),
-  };
-
+export class AccessMethod extends Base {
   template() {
-    const { Condition } = this.Props;
-    return html`
-      <div class="Condition">
-        ${Condition.input()}
-        ${this.deleteButton({ title: "Delete this condition" })}
-      </div>
-    `;
+    return html`<div class="access-method treebase">
+      ${Globals.method.template()}
+    </div>`;
   }
 }
-TreeBase.register(HandlerCondition);
 
-const allResponses = {
-  next: () => Globals.pattern.next(),
-  activate: () => Globals.pattern.activate(),
-};
-
-class HandlerResponse extends TreeBase {
+export class MethodChooser extends TreeBase {
   props = {
-    Response: new Select(Object.keys(allResponses), { hiddenLabel: true }),
+    currentIndex: new Integer(),
   };
+  /** @type {Method} */
+  currentMethod = null;
+
+  /** @type {Method[]} */
+  children = [];
+
+  update() {
+    db.write("method", this.toObject());
+    Globals.state.update();
+  }
 
   template() {
-    const { Response } = this.Props;
-    return html`
-      <div class="Condition">
-        ${Response.input()}
-        ${this.deleteButton({ title: "Delete this response" })}
-      </div>
-    `;
+    /** @type {string[]} */
+    const choices = this.children.map((child) => child.Props.Name.value);
+    console.log({ choices });
+    const { currentIndex } = this.Props;
+    if (currentIndex.value >= 0 && currentIndex.value < choices.length) {
+      this.currentMethod = this.children[currentIndex.value];
+    } else if (choices.length > 0) {
+      currentIndex.value = choices.length - 1;
+      this.currentMethod = this.children[currentIndex.value];
+    }
+    return html`<div class="MethodChooser" onChange=${() => this.update()}>
+      <label
+        >Access Method
+        <select
+          onChange=${(/** @type {{ target: { value: string; }; }} */ e) => {
+            currentIndex.value = parseInt(e.target.value);
+            this.currentMethod = this.children[currentIndex.value];
+          }}
+        >
+          ${choices.map(
+            (label, index) =>
+              html`<option
+                value=${index}
+                ?selected=${currentIndex.value == index}
+              >
+                ${label}
+              </option>`
+          )}
+        </select></label
+      >
+      ${this.addChildButton("+Method", Method, {
+        title: "Create a new access method",
+        onClick: () => currentIndex.set(this.children.length - 1),
+      })}
+      ${this.currentMethod ? this.currentMethod.template() : html``}
+    </div> `;
   }
 }
-TreeBase.register(HandlerResponse);
+TreeBase.register(MethodChooser);
+
+class Method extends TreeBase {
+  props = {
+    Name: new String("New method"),
+  };
+
+  /** @type {Handler[]} */
+  children = [];
+
+  template() {
+    const { Name } = this.Props;
+    return html`<fieldset class="Method">
+      <legend>${Name.value}</legend>
+      ${Name.input()}
+      ${this.addChildButton("+Handler", Handler, { title: "Add a handler" })}
+      ${this.orderedChildren()}
+    </fieldset>`;
+  }
+}
+TreeBase.register(Method);
 
 const allSignals = ["keyup", "keydown"];
 
@@ -96,86 +141,41 @@ class Handler extends TreeBase {
 }
 TreeBase.register(Handler);
 
-class Method extends TreeBase {
+class HandlerCondition extends TreeBase {
   props = {
-    Name: new String("New method"),
+    Condition: new Expression("", { hiddenLabel: true }),
   };
 
-  /** @type {Handler[]} */
-  children = [];
-
   template() {
-    const { Name } = this.Props;
-    return html`<fieldset class="Method">
-      <legend>${Name.value}</legend>
-      ${Name.input()}
-      ${this.addChildButton("+Handler", Handler, { title: "Add a handler" })}
-      ${this.orderedChildren()}
-    </fieldset>`;
+    const { Condition } = this.Props;
+    return html`
+      <div class="Condition">
+        ${Condition.input()}
+        ${this.deleteButton({ title: "Delete this condition" })}
+      </div>
+    `;
   }
 }
-TreeBase.register(Method);
+TreeBase.register(HandlerCondition);
 
-export class MethodChooser extends TreeBase {
+const allResponses = {
+  next: () => Globals.pattern.next(),
+  activate: () => Globals.pattern.activate(),
+};
+
+class HandlerResponse extends TreeBase {
   props = {
-    currentIndex: new Integer(),
+    Response: new Select(Object.keys(allResponses), { hiddenLabel: true }),
   };
-  /** @type {Method} */
-  currentMethod = null;
-
-  /** @type {Method[]} */
-  children = [];
-
-  update() {
-    db.write("method", this.toObject());
-    Globals.state.update();
-  }
 
   template() {
-    /** @type {string[]} */
-    const choices = this.children.map((child) => child.Props.Name.value);
-    console.log({ choices });
-    const { currentIndex } = this.Props;
-    if (currentIndex.value >= 0 && currentIndex.value < choices.length) {
-      this.currentMethod = this.children[currentIndex.value];
-    } else if (choices.length > 0) {
-      currentIndex.value = choices.length - 1;
-      this.currentMethod = this.children[currentIndex.value];
-    }
-    return html`<div class="MethodChooser" onChange=${() => this.update()}>
-      <label
-        >Access Method
-        <select
-          onChange=${(e) => {
-            currentIndex.value = parseInt(e.target.value);
-            this.currentMethod = this.children[currentIndex.value];
-          }}
-        >
-          ${choices.map(
-            (label, index) =>
-              html`<option
-                value=${index}
-                ?selected=${currentIndex.value == index}
-              >
-                ${label}
-              </option>`
-          )}
-        </select></label
-      >
-      ${this.addChildButton("+Method", Method, {
-        title: "Create a new access method",
-        onClick: () => currentIndex.set(this.children.length - 1),
-      })}
-      ${this.currentMethod ? this.currentMethod.template() : html``}
-    </div> `;
+    const { Response } = this.Props;
+    return html`
+      <div class="Condition">
+        ${Response.input()}
+        ${this.deleteButton({ title: "Delete this response" })}
+      </div>
+    `;
   }
 }
-TreeBase.register(MethodChooser);
-
-export class AccessMethod extends Base {
-  template() {
-    return html`<div class="access-method treebase">
-      ${Globals.method.template()}
-    </div>`;
-  }
-}
+TreeBase.register(HandlerResponse);
