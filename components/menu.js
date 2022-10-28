0d9439f808ce404c352c599c41eb072ba2dc4e8e
Gary Bishop <gary.bishop.unc@gmail.com>
2022-10-28 11:55:37 -0400

    Added more key controls.

--- components/menu.js
+++ components/menu.js
@@ -22,76 +22,31 @@ export class Menu {
 
   expanded = false;
 
+  /** @type {MenuItem[]} */
+  items = []; // cached items returned from the contentCallback
+
+  /** @type {HTMLElement} - reference to the outer div */
+  current = null;
+
   /** @param {string} label - label on the menu button
    * @param {function(): MenuItem[]} contentCallback - returns the menu items to display
    */
   constructor(label, contentCallback) {
     this.label = label;
     this.contentCallback = contentCallback;
-
-    /* Close the menu when it loses focus */
-    this.checkFocusOut = ({ relatedTarget }) => {
-      console.log("checkFocusOut", relatedTarget, this.expanded);
-      if (!relatedTarget) {
-        callAfterRender(() => {
-          const button = document.getElementById(this.buttonId);
-          if (button) button.focus();
-        });
-        if (this.expanded) this.toggleExpanded();
-        return;
-      }
-      const menu = document.getElementById(this.id);
-      if (menu && !menu.contains(relatedTarget) && this.expanded) {
-        this.toggleExpanded();
-      }
-    };
-
-    /* Toggle the menu state */
-    this.toggleExpanded = (event = null) => {
-      {
-        this.expanded = !this.expanded;
-        if (this.expanded && event && event.detail === 0) {
-          // focus on the first element when expanding via keyboard
-          callAfterRender(() => {
-            const ul = document.getElementById(this.contentId);
-            const first = ul.querySelector("button");
-            first.focus();
-          });
-        }
-        Globals.state.update();
-      }
-    };
-
-    this.keyHandler = ({ key }) => {
-      if (key == "Escape") this.toggleExpanded();
-      else if (key == "ArrowUp" || key == "ArrowDown") {
-        const menu = document.getElementById(this.contentId);
-        const items = [...menu.querySelectorAll("button")];
-        const current = /** @type {HTMLButtonElement} */ (
-          menu.querySelector("button:focus")
-        );
-        if (current) {
-          const index = items.indexOf(current);
-          const step = key == "ArrowUp" ? -1 : 1;
-          const count = items.length;
-          const next = (index + step + count) % count;
-          items[next].focus();
-        }
-      }
-    };
   }
 
   render() {
-    let items = [];
     if (this.expanded) {
-      items = this.contentCallback();
-      console.log({ items });
+      this.items = this.contentCallback();
+    } else {
+      this.items = [];
     }
     return html`<div
       class="Menu"
       id=${this.id}
-      onkeyup=${this.keyHandler}
-      onfocusout=${this.checkFocusOut}
+      onfocusout=${this.focusHandler}
+      ref=${this}
     >
       <button
         id=${this.buttonId}
@@ -99,6 +54,7 @@ export class Menu {
         aria-controls=${this.contentId}
         aria-haspopup="true"
         onclick=${this.toggleExpanded}
+        onkeyup=${this.buttonKeyHandler}
       >
         ${this.label}
       </button>
@@ -107,11 +63,13 @@ export class Menu {
         role="menu"
         id=${this.contentId}
         aria-labelledby=${this.buttonId}
+        onkeyup=${this.menuKeyHandler}
       >
-        ${items.map(
-          (item) =>
+        ${this.items.map(
+          (item, index) =>
             html`<li role="menuitem">
               <button
+                index=${index}
                 onclick=${() => {
                   this.toggleExpanded();
                   item.callback();
@@ -124,6 +82,96 @@ export class Menu {
       </ul>
     </div>`;
   }
+
+  /** @returns {HTMLButtonElement} */
+  get focusedItem() {
+    return this.current.querySelector("li > button:focus");
+  }
+
+  /** @param {number} index */
+  setFocus(index) {
+    index = (index + this.items.length) % this.items.length;
+    const item = /** @type {HTMLElement} */ (
+      this.current.querySelector(`button[index="${index}"]`)
+    );
+    item.focus();
+  }
+
+  /* Close the menu when it loses focus */
+  focusHandler = ({ relatedTarget }) => {
+    if (!relatedTarget) {
+      // focus is gone, put it back on the button
+      callAfterRender(() => {
+        const button = document.getElementById(this.buttonId);
+        if (button) button.focus();
+      });
+      if (this.expanded) this.toggleExpanded();
+      return;
+    }
+    const menu = document.getElementById(this.id);
+    if (menu && !menu.contains(relatedTarget) && this.expanded) {
+      this.toggleExpanded();
+    }
+  };
+
+  /* Toggle the menu state */
+  toggleExpanded = (event = null, last = false) => {
+    {
+      this.expanded = !this.expanded;
+      // this trick lets us distinguish between clicking the menu button with the mouse
+      // and hitting Enter on the keyboard
+      const mouseClick = event && event.detail !== 0;
+      if (this.expanded && (!event || !mouseClick)) {
+        // focus on the first element when expanding via keyboard
+        callAfterRender(() => {
+          if (last) {
+            this.setFocus(-1);
+          } else {
+            this.setFocus(0);
+          }
+        });
+      }
+      Globals.state.update();
+    }
+  };
+
+  menuKeyHandler = ({ key }) => {
+    if (key == "Escape" && this.expanded) {
+      this.toggleExpanded();
+    } else if (key == "ArrowUp" || key == "ArrowDown") {
+      const focused = this.focusedItem;
+      const index = +focused.getAttribute("index");
+      const step = key == "ArrowUp" ? -1 : 1;
+      this.setFocus(index + step);
+    } else if (key == "Home") {
+      this.setFocus(0);
+    } else if (key == "End") {
+      this.setFocus(-1);
+    } else if (
+      key.length == 1 &&
+      ((key >= "a" && key <= "z") || (key >= "A" && key <= "Z"))
+    ) {
+      const focused = this.focusedItem;
+      const index = +focused.getAttribute("index");
+      for (let i = 1; i < this.items.length; i++) {
+        if (this.items[(index + i) % this.items.length].label.startsWith(key)) {
+          this.setFocus(i + index);
+          break;
+        }
+      }
+    }
+  };
+
+  /** @param {KeyboardEvent} event */
+  buttonKeyHandler = (event) => {
+    if (event.key == "ArrowDown" || event.key == " ") {
+      event.preventDefault();
+      this.toggleExpanded();
+    } else if (event.key == "ArrowUp") {
+      event.preventDefault();
+      this.toggleExpanded(null, true);
+    }
+  };
 }
 
 css`
