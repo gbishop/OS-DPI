0d3a437968af6e1514849d660698884fce170106
Gary Bishop <gary.bishop.unc@gmail.com>
2023-06-16 15:48:07 -0400

    Enable reconnection.

--- components/access/method/socketHandler.js
+++ components/access/method/socketHandler.js
@@ -36,47 +36,86 @@ export class SocketHandler extends Handler {
     // TODO: figure out how to remove these or make them weak
     Globals.state.observe(() => {
       if (Globals.state.hasBeenUpdated(this.StateName.value)) {
-        if (this.socket) {
-          // send the entire state over to the server
-          this.socket.next(Globals.state.values);
+        if (!this.socket) {
+          // the connect wasn't successfully opened, try again
+          this.reconfigure();
         }
+        if (!this.live) {
+          // if the connection was shut down completion then resubscribe to reopen it.
+          this.subscribe();
+        }
+        if (!this.socket || !this.live) return;
+        // send the data over the websocket
+        this.socket.next(Globals.state.values);
       }
     });
   }
 
-  /** @type {import("rxjs/webSocket").WebSocketSubject<any>} */
-  socket;
+  // true when the socket exists and subscribed
+  live = false;
+
+  /** The websocket wrapper object
+   * @type {import("rxjs/webSocket").WebSocketSubject<any> | undefined} */
+  socket = undefined;
+
+  /** The stream of events from the websocket
+   * @type {RxJs.Observable<Event & { access: {}}> | undefined} */
+  socket$ = undefined;
+
+  /** Save the stop$ subject so we can use it when reconfiguring
+   * @type {RxJs.Subject | undefined} */
+  savedStop$ = undefined;
 
   /** @param {RxJs.Subject} stop$ */
   configure(stop$) {
+    this.savedStop$ = stop$;
+    this.reconfigure();
+  }
+
+  reconfigure() {
+    if (!this.savedStop$) return; // keeping type checking happy
     const { conditions } = this;
+    // this is the socket object
     this.socket = webSocket(this.URL.value);
-    this.socket
-      .pipe(
-        RxJs.map((msg) => {
-          const event = new Event("socket");
-          const wrapped = EventWrap(event);
-          wrapped.access = msg;
-          return wrapped;
-        }),
-        RxJs.filter(
-          (e) =>
-            conditions.length == 0 ||
-            conditions.every((condition) => condition.Condition.eval(e.access))
-        ),
-        RxJs.takeUntil(stop$)
-      )
-      .subscribe({
-        next: (e) => {
-          this.respond(e);
-        },
-        error(err) {
-          console.error(err);
-        },
-        complete() {
-          console.log("complete");
-        },
-      });
+    // this is the stream of events from it
+    this.socket$ = this.socket.pipe(
+      RxJs.map((msg) => {
+        const event = new Event("socket");
+        const wrapped = EventWrap(event);
+        wrapped.access = msg;
+        return wrapped;
+      }),
+      RxJs.filter(
+        (e) =>
+          conditions.length == 0 ||
+          conditions.every((condition) => condition.Condition.eval(e.access))
+      ),
+      RxJs.takeUntil(this.savedStop$)
+    );
+    this.subscribe();
+  }
+
+  subscribe() {
+    if (!this.socket$) return;
+    this.live = true;
+    this.socket$.subscribe({
+      next: (e) => {
+        // pass incoming messages to the response
+        this.respond(e);
+      },
+      error: (err) => {
+        // force a call to reconfigure
+        console.error("error", err);
+        this.live = false;
+        this.socket = undefined;
+        this.socket$ = undefined;
+      },
+      complete: () => {
+        // mark that it is not live but it could be restarted
+        console.log("complete");
+        this.live = false;
+      },
+    });
   }
 }
 TreeBase.register(SocketHandler, "SocketHandler");
