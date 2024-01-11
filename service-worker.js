cb82e877ba674f4661be8668eae64695d64fef19
Gary Bishop <gary.bishop.unc@gmail.com>
2024-01-11 13:10:30 -0500

    deploy

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 var GHPATH = "/OS-DPI";
 var APP_PREFIX = "osdpi_";
-var VERSION = "2024-0-11-12-23-42";
+var VERSION = "2024-0-11-13-10-27";
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
